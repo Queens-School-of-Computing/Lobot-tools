50bba7de9ce8665c4f8eb117f7e6e567768997e4
Aaron Visser <aaron.visser@gmail.com>
2026-04-30 21:07:22 -0400

    fix: typo in email

--- auto-pull-images.sh
+++ auto-pull-images.sh
@@ -12,8 +12,8 @@ SMTP_PORT=25
 SMTP_USE_TLS=false
 SMTP_USERNAME=""
 SMTP_PASSWORD=""
-FROM_EMAIL="lobot-toolscs.queensu.ca"
-TO_EMAIL="aaron@cs.queensu.ca"
+FROM_EMAIL="lobot-tools@cs.queensu.ca"
+TO_EMAIL="aaron.visser@queensu.ca"
 
 DRY_RUN=false
 
